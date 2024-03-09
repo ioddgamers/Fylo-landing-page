46d3f2aed1b4b1ad07508ea0dc3b28258c8bcb3f
Tolulope <toluloperbrts@gmail.com>
2024-03-09 17:38:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const firstEmailInput = document.getElementById('first-email');
+const secondEmailInput = document.getElementById('second-email');
+const error1 = document.getElementById('first-error');
+const error2 = document.getElementById('second-error');
+const button = document.querySelector('.btn');
+const button2 = document.querySelector('.second-btn');
+
+button.addEventListener('click', firstEmailValidation);
+
+button2.addEventListener('click', secondEmailValidation);
+
+function firstEmailValidation() {
+    
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const email = firstEmailInput.value;
+
+    if(emailRegex.test(email)) {
+        error1.classList.add('hidden');
+        firstEmailInput.style.borderColor = "black";
+    } else {
+        error1.classList.remove('hidden');
+        firstEmailInput.style.borderColor = "red";
+    }
+}
+
+function secondEmailValidation() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const email = secondEmailInput.value;
+
+    if(emailRegex.test(email)) {
+        error2.classList.add('hidden2');
+    } else {
+        error2.classList.remove('hidden2');
+    }
+}
